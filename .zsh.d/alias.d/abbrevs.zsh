d51f6bfce316b9e0063cc6b3bac39d71e54d8474
Adrian Pistol <vifino@tty.sh>
2017-11-22 04:27:40 +0100

    zsh: More abbreviations.

--- .zsh.d/alias.d/abbrevs.zsh
+++ .zsh.d/alias.d/abbrevs.zsh
@@ -1,7 +1,20 @@
-# Short abbreviations
+##
+# Short abbreviations and helpers
+##
+
+# Helpers
+silent() {
+	[ -z "$1" ] && echo "Usage: silent prog [args..]" && return 1
+
+	local prog="$1" resolved
+	resolved=$(whence "$1")
+	[ $? = 0 ] && prog="$resolved"
+	"$prog" "$@" &> /dev/null
+}
 
 # Generic
 alias h=hostname
+alias wh=which
 
 # Applications
 alias t=tmux
@@ -19,3 +32,10 @@ p() { # Pager
 		"$PAGER" "$@"
 	fi
 }
+
+# gstreamer
+silent wh gst-launch-1.0 && alias gst=gst-launch-1.0 ||
+[ $? != 0 ] && silent wh gst-launch-0.1 && alias gst=gst-launch-0.1
+
+# X11/WM stuff
+silent wh herbstclient && alias hc=herbstclient
