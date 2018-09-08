6264156ae08ea9f677b6c2bb36cf4cf0fd6bcb7c
Adrian Pistol <vifino@tty.sh>
2018-09-08 15:26:10 +0200

    herp derp trains

--- .zsh.d/hosts/punch/00-cc-env.zsh
+++ .zsh.d/hosts/punch/00-cc-env.zsh
@@ -1,5 +1,6 @@
 #!/usr/bin/env zsh
 
+export CFLAGS="-O2 -march=native"
 export CC=clang CPP=clang-cpp CXX=clang++
 export CPPFLAGS="-I/usr/local/include -I/usr/X11R6/include" 
 export LDFLAGS="-L/usr/local/lib -L/usr/X11R6/lib"
--- .zsh.d/setup.d/pager.zsh
+++ .zsh.d/setup.d/pager.zsh
@@ -1,5 +1,7 @@
 #!/usr/bin/env zsh
 # set PAGER variable appropriately
+# also sets some options
+export LESS="-+F -R -+E -X"
 
 for pager in most less; do
 	silent which $pager && export PAGER=$pager && return
